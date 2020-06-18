00b5b19adcf1557f534624da2d4fb6a837020909
Ali.NET <aliemami.net@gmail.com>
2020-06-18 23:17:17 +0430

    SHN-0061: Fix Summernote and Admin Interface app translate

--- ShenasaSolution/settings.py
+++ ShenasaSolution/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from django.utils.translation import ugettext_lazy as _
 
-VERSION = '0.45.6'
+VERSION = '0.45.7'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
